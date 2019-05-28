df5e635915f1349ebcab6088452466e02389aa6f
Oliveira Reboucas <oliveirareboucasc@student.douglascollege.ca>
2019-05-28 14:38:51 -0700

    initial commit

---
+++ src/main/java/com/PostTracking/PostTracking/PostTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.PostTracking.PostTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostTrackingApplication.class, args);
+	}
+
+}
