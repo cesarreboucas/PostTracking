e4718a877dd268d1acae305cc2180610565f8a5c
cesarreboucas <cesar.reboucas@gmail.com>
2019-05-31 01:59:42 -0700

    Starting Packages Controller

--- src/main/java/com/PostTracking/Controllers/PackageController.java
+++ src/main/java/com/PostTracking/Controllers/PackageController.java
@@ -1,24 +1,38 @@
 package com.PostTracking.Controllers;
 
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 import com.PostTracking.Entities.Package;
 
 @Controller
 public class PackageController {
 	
 	@GetMapping("/packages")
-	public String getAll() {
+	public String showAll() {
 		return "packages/packages";
 	}
 	
-	/*public ArrayList<Package> getAll() {
+	@ModelAttribute("packages")
+	public ArrayList<Package> getAll() {
+		
 		ArrayList<Package> list = new ArrayList<Package>();
 		list.add(new Package(2.0,2.0));
 		list.add(new Package(3.0,3.0));
 		list.add(new Package(4.0,4.0));
 		list.add(new Package(5.0,5.0));
+	
+		return list;
+		
+		
+	}
+	
+	/*public ArrayList<Package> getAll() {
+		
 		
 		return list;
 	}*/
