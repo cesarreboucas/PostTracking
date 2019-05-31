d9d74616f02c662524e1587a69a779ddd886b9f8
Oliveira Reboucas <oliveirareboucasc@student.douglascollege.ca>
2019-05-30 17:14:06 -0700

    Configuring fragments thymeleaf

--- src/main/java/com/PostTracking/Controllers/IndexController.java
+++ src/main/java/com/PostTracking/Controllers/IndexController.java
@@ -1,22 +1,16 @@
 package com.PostTracking.Controllers;
 
 
-import java.util.ArrayList;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class IndexController {
 
 	@GetMapping("/")
-	public ArrayList<String> home(  ) {
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("A");
-		list.add("B");
-		list.add("C");
-		list.add("D");
-		return list;
+	public String home(  ) {
+		return "index/index";
 		
 	}
 }
