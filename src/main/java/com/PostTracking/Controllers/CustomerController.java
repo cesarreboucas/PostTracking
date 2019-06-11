2a5b907e1f6598d997a78df84f95294b86c03f3a
Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2019-06-11 15:48:47 -0700

    Updated Customer Controller

--- src/main/java/com/PostTracking/Controllers/CustomerController.java
+++ src/main/java/com/PostTracking/Controllers/CustomerController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ClientController {
+public class CustomerController {
 
-	@GetMapping("/clients")
+	@GetMapping("/customers")
 	public String ShowAll() {
-		return "clients/clients";
+		return "customers/customers";
 		
 	}
 	
