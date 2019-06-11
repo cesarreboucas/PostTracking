0d0a3180875883721b1443cd380dcbc39b2842d5
Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2019-06-11 15:49:20 -0700

    Updated Distribution Center (Entity)

--- src/main/java/com/PostTracking/Entities/DistributionCenter.java
+++ src/main/java/com/PostTracking/Entities/DistributionCenter.java
@@ -2,19 +2,65 @@ package com.PostTracking.Entities;
 
 public class DistributionCenter {
 
-    private String name;
-    private int id;
+	private int id = 0;
+	private String name = "";
+	private String address = "";
+	private String city = "";
+	private String province = "";
+	private String zipCode = "";
+    
 
     public DistributionCenter(int id, String name) {
-        this.name = name;
-        this.id = id;
+    	this.id = id;
+    	this.name = name;
     }
 
+	public int getId() {
+        return this.id;
+    }
+    
+    public void setId(int id ) {
+    	this.id = id;
+    }
+    
     public String getName() {
         return this.name;
     }
     
-    public int getId() {
-        return this.id;
+    public void setName(String name) {
+    	this.name = name;
     }
+    
+    public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+    
 }
