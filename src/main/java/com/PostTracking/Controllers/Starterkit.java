9208fd8b0270566ec738a17c6257de49cd0f7f15
Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2019-06-17 18:23:33 -0700

    Added Customers to database (starterkit)

--- src/main/java/com/PostTracking/Controllers/Starterkit.java
+++ src/main/java/com/PostTracking/Controllers/Starterkit.java
@@ -18,6 +18,8 @@ import com.PostTracking.Entities.DistributionCenter;
 import com.PostTracking.Entities.Route;
 import com.PostTracking.Entities.Vehicle;
 
+// https://mockaroo.com/
+
 @Controller
 public class Starterkit {
 	
@@ -70,6 +72,26 @@ public class Starterkit {
 		/**************** Customer **************/
 		ArrayList<Customer> cArrayList = new ArrayList<Customer>(); 
 		cArrayList.add(new Customer("Jon", "Snow", "123-456-7890", "jon.snow@winter.com", "Castle Black", "Frickin Cold", "Westeros", "S0o-F4R"));
+		cArrayList.add(new Customer("Barbara", "McRill", "898-569-6799", "bmcrill0@mozilla.com", "27 Bultman Alley", "Maple Ridge", "British Columbia", "V2X"));
+		cArrayList.add(new Customer("Jonathon", "Kennermann", "605-284-5565", "jkennermann1@de.vu", "47672 Jenifer Park", "North Vancouver", "British Columbia", "K1B"));
+		cArrayList.add(new Customer("Emmery", "Vittery", "620-146-8347", "evittery2@prnewswire.com", "32291 Sachs Parkway", "Delta", "British Columbia", "M2J"));
+		cArrayList.add(new Customer("Suzette", "Pietz", "710-112-4229", "spietz3@gmpg.org", "3777 Fulton Trail", "Langley", "British Columbia", "H1V"));
+		cArrayList.add(new Customer("Mickey", "Loughan", "371-971-6381", "mloughan4@wordpress.com", "88 Loomis Lane", "Burnaby", "British Columbia", "V4C"));
+		cArrayList.add(new Customer("Marthena", "Pestor", "767-914-3602", "mpestor5@hp.com", "4 Bartelt Drive", "Delta", "British Columbia", "H9G"));
+		cArrayList.add(new Customer("Jeffrey", "Eisikovitsh", "111-486-1930", "jeisikovitsh6@google.pl", "970 Holy Cross Pass", "Vancouver", "British Columbia", "H1V"));
+		cArrayList.add(new Customer("Elfrida", "Rooma", "890-897-4809", "erooma7@vk.com", "5 Summit Alley", "Langley", "British Columbia", "H1V"));
+		cArrayList.add(new Customer("Annadiana", "Gantz", "205-933-7071", "agantz8@ameblo.jp", "26 Ronald Regan Avenue", "Langley", "British Columbia", "V9G"));
+		cArrayList.add(new Customer("Moe", "Dishmon", "349-877-4588", "mdishmon9@netvibes.com", "56801 Westerfield Street", "Langley", "British Columbia", "V2A"));
+		cArrayList.add(new Customer("Lindsey", "Leavey", "907-229-1353", "lleaveya@cisco.com", "3 Sycamore Place", "North Vancouver", "British Columbia", "V9A"));
+		cArrayList.add(new Customer("Adilson", "Hill", "257-401-8656", "ahill@wired.com", "2 Sullivan Street", "Coquitlam", "British Columbia", "V9P"));
+		cArrayList.add(new Customer("Leighton", "Jeannard", "830-331-5515", "ljeannardc@dot.gov", "0 Troy Avenue", "North Vancouver", "British Columbia", "V9C"));
+		cArrayList.add(new Customer("Venita", "Fison", "808-411-6301", "vfisond@admin.ch", "0720 5th Avenue", "Delta", "British Columbia", "G2L"));
+		cArrayList.add(new Customer("Orbadiah", "Bly", "755-505-1253", "oblye@hatena.ne.jp", "3863 Rowland Drive", "Coquitlam", "British Columbia", "E1W"));
+		cArrayList.add(new Customer("Justine", "Gives", "793-964-3078", "jgivesf@symantec.com", "8490 Truax Terrace", "Coquitlam", "British Columbia", "L9T"));
+		cArrayList.add(new Customer("Markos", "Oki", "889-612-4366", "mokig@liveinternet.ru", "168 Wayridge Place", "Maple Ridge", "British Columbia", "M2J"));
+		cArrayList.add(new Customer("Dante", "Bispo", "521-104-6254", "dbispoh@msn.com", "24 Grasskamp Junction", "Richmond", "British Columbia", "V7Y"));
+		cArrayList.add(new Customer("Elana", "Tripet", "328-662-7976", "etripeti@cocolog-nifty.com", "44 Meadow Vale Parkway", "Vancouver", "British Columbia", "V6L"));
+		cArrayList.add(new Customer("Eduard", "Catterell", "360-163-7077", "ecatterellj@who.int", "0 Sherman Hill", "North Vancouver", "British Columbia", "M2J"));
 		for(Customer c : cArrayList) {
 			cDAO.createCustomer(c);
 		}
