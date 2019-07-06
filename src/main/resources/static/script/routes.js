867e24b8af8ae89665f41bd2dd83af166b579393
Marcos Eduardo Marangoni <marcoseduardomarangoni@Marcoss-iMac.hitronhub.home>
2019-07-05 21:26:16 -0700

    Almost done with the validation. Then is needed to do the controller.

--- src/main/resources/static/script/routes.js
+++ src/main/resources/static/script/routes.js
@@ -2,8 +2,6 @@
 const routeTableHeaders = ['Origin', 'Destination', 'Start', 'Arrival'];
 let vehicles = [];
 let distributionCenters = [];
-let tmpDistributionCenters = [];
-let routes = [];
 let selectedVehicle = null;
 
 const getVehicles = new Promise((resolve, reject) => {
@@ -42,24 +40,28 @@ function clearFormContainer() {
 }
 
 function createModal(event) {
-    clearFormContainer();
     Promise.all([getVehicles, getDistributionCenters])
     .then((data) => {
         vehicles = data[0];
         distributionCenters = data[1];
+        selectedVehicle = null;
         console.log('[VEHICLES]', vehicles);
         console.log('[DISTRIBUTION_CENTER]', distributionCenters);
-        createDropDown();
+        createCard();
     })
     .catch(onFetchFail);
 }
 
+function onFetchFail(error) {
+    console.log('[ERROR]', error);
+}
+
 function createDropDown() {
-    let formContainer = document.getElementById('FormContainer');
+    //let formContainer = document.getElementById('FormContainer');
 
     let vehiclesDropDown = document.createElement('div');
     vehiclesDropDown.setAttribute('class', 'btn-group');
-    formContainer.appendChild(vehiclesDropDown);
+    //formContainer.appendChild(vehiclesDropDown);
 
     let vehiclesDropDownTrigger = document.createElement('button');
     vehiclesDropDownTrigger.setAttribute('type', 'button');
@@ -67,7 +69,8 @@ function createDropDown() {
     vehiclesDropDownTrigger.setAttribute('data-toggle', 'dropdown');
     vehiclesDropDownTrigger.setAttribute('aria-haspopup', 'true');
     vehiclesDropDownTrigger.setAttribute('aria-expanded', 'false');
-    vehiclesDropDownTrigger.textContent = 'Selecione o veiculo';
+    vehiclesDropDownTrigger.textContent = 'Select a vehicle';
+    vehiclesDropDownTrigger.id = 'DropDownTrigger';
     vehiclesDropDown.appendChild(vehiclesDropDownTrigger);
 
     let vehiclesDropDownMenu = document.createElement('div');
@@ -82,13 +85,12 @@ function createDropDown() {
         vehiclesDropDownMenuItem.textContent = vehicle.description;
         vehiclesDropDownMenu.appendChild(vehiclesDropDownMenuItem);
     });
-}
 
-function onFetchFail(error) {
-    console.log('[ERROR]', error);
+    return vehiclesDropDown;
 }
 
 function onDropDownMenuClick(event) {
+    const dropDownTrigger = document.getElementById('DropDownTrigger');
     const element = event.srcElement;
     const vehicleId = parseInt(element.getAttribute('data-vehicleId'));
     vehicles.forEach(vehicle => {
@@ -97,13 +99,10 @@ function onDropDownMenuClick(event) {
             return;
         }
     });
-    console.log('[SELECTED_VEHICLE]', selectedVehicle);
-    clearFormContainer()
-    createCard();
+    dropDownTrigger.textContent = selectedVehicle.description;
 }
 
 function createCard() {
-    tmpDistributionCenters = [...distributionCenters];
     let formContainer = document.getElementById('FormContainer');
 
     let card = document.createElement('div');
@@ -112,7 +111,7 @@ function createCard() {
 
     let cardHeader = document.createElement('div');
     cardHeader.setAttribute('class', 'card-header');
-    cardHeader.textContent = selectedVehicle.description;
+    cardHeader.appendChild(createDropDown());
     card.appendChild(cardHeader);
 
     let cardBody = document.createElement('div');
@@ -134,212 +133,150 @@ function createCard() {
     });
 
     createRow();
-    routes.push({
-        vehicleId: selectedVehicle.id,
-        originName: '',
-        originId: -1,
-        destinationName: '',
-        destinationId: -1,
-        dateOrigin: 0,
-        dateDestination: 0
-    });
 }
 
 function createRow() {
-    let routeTable = document.getElementById('RouteTable');
-
     tr = document.createElement('tr');
-    routeTable.appendChild(tr);
 
     let td = document.createElement('td');
-    let originSelect = createSelect('OriginSelect');
+    let originSelect = createSelect();
     td.appendChild(originSelect);
     tr.appendChild(td);
 
     td = document.createElement('td');
-    let destinationSelect = createSelect('DestinationSelect');
+    let destinationSelect = createSelect();
     td.appendChild(destinationSelect);
     tr.appendChild(td);
-
-    const idIndex = routes.length;
 
     td = document.createElement('td');
     let originDate = document.createElement('input');
     originDate.setAttribute('type', 'datetime-local');
-    originDate.setAttribute('id', `OriginDate${idIndex}`);
     td.appendChild(originDate);
     tr.appendChild(td);
 
     td = document.createElement('td');
     let destinationDate = document.createElement('input');
     destinationDate.setAttribute('type', 'datetime-local');
-    destinationDate.setAttribute('id', `DestinationDate${idIndex}`);
     td.appendChild(destinationDate);
-    tr.appendChild(td);
-
-    td = document.createElement('td');
-    let buttonPlus = document.createElement('a');
-    buttonPlus.setAttribute('class', 'btn btn-default btn-sm');
-    buttonPlus.textContent = '+';
-    buttonPlus.dataset['routeId'] = idIndex;
-    buttonPlus.addEventListener('click', onAddTableRow)
-    td.appendChild(buttonPlus);
     tr.appendChild(td);
 
     td = document.createElement('td');
     let buttonRemove = document.createElement('a');
     buttonRemove.setAttribute('class', 'btn btn-default btn-sm');
     buttonRemove.textContent = '-';
-    buttonRemove.dataset['routeId'] = idIndex;
     buttonRemove.addEventListener('click', onRemoveRableRow);
     td.appendChild(buttonRemove);
     tr.appendChild(td);
+
+    const routeTable = document.getElementById('RouteTable');
+    routeTable.appendChild(tr);
 }
 
 function onAddTableRow(event) {
     const element = event.srcElement;
-    const idIndex = element.dataset['routeId'];
-
-    const selectOrigin = document.getElementById(`OriginSelect${idIndex}`);
-    let selectOriginValue;
-    if(selectOrigin.tagName === 'SELECT') {
-        selectOriginValue = parseInt(selectOrigin.options[selectOrigin.selectedIndex].value);
-    } else {
-        selectOriginValue = parseInt(selectOrigin.dataset['dcId']);
-    }
-    const selectOriginText = selectOrigin.options[selectOrigin.selectedIndex].text;
-    console.log('[SELECT_ORIGIN_VALUE]', selectOriginValue);
-
-    const selectDestination = document.getElementById(`DestinationSelect${idIndex}`);
-    let selectDestinationValue;
-    if(selectDestination.tagName === 'SELECT') {
-        selectDestinationValue = parseInt(selectDestination.options[selectDestination.selectedIndex].value);
-    } else {
-        selectDestinationValue = parseInt(selectDestination.dataset['dcId']);
-    }
-
-    const selectDestinationText = selectDestination.options[selectDestination.selectedIndex].text;
-    
-    const dateOrigin = document.getElementById(`OriginDate${idIndex}`);
-    const dateOriginValue = dateOrigin.value;
-    let dateOriginDate = Date.parse(dateOriginValue) / 1000;
-    console.log('[DATE_ORIGIN_DATE]', dateOriginDate);
-
-    const dateDestination = document.getElementById(`DestinationDate${idIndex}`);
-    const dateDestinationValue = dateDestination.value;
-    let dateDestinationDate = Date.parse(dateDestinationValue) / 1000;
-
-
-    if(selectOriginValue === selectDestinationValue) {
-        alert('Destination should be different than origin!');
-        return;
-    }
-    if(!dateOriginValue || !dateDestinationValue) {
-        alert('Please input valid dates');
-        return;
-    }
-    if(dateOriginDate >= dateDestinationDate) {
-        alert('Origin date should be smaller than arrival date');
-        return;
-    }
-    if(routes.length > 1) {
-        const index = routes.length - 1;
-        if(dateOriginDate <= routes[index - 1].dateDestination) {
-            alert('Origin date should be bigger than last arrival');
-            return;
-        }
-    }
-
-    console.log('[VALIDATION_PASSED!]');
-
-    let route = routes[routes.length - 1];
-    route.vehicleId = selectedVehicle.id;
-    route.originName = selectOriginText;
-    route.originId = selectOriginValue;
-    route.destinationName = selectDestinationText;
-    route.destinationId = selectDestinationValue;
-    route.dateOrigin = dateOriginDate;
-    route.dateDestination = dateDestinationDate;
-
-    const indexToRemoveOrigin = tmpDistributionCenters.findIndex(distributionCenter => distributionCenter.id === selectDestinationValue);
-    const indextoRemoveDestination = tmpDistributionCenters.findIndex(distributionCenter => distributionCenter.id === selectOriginValue);
-    if(indexToRemoveOrigin >= 0) tmpDistributionCenters.splice(indexToRemoveOrigin, 1);
-    if(indextoRemoveDestination >= 0) tmpDistributionCenters.splice(indextoRemoveDestination, 1);
-    console.log('[TMP_DISTRIBUTION]', tmpDistributionCenters);
-
-    createRow();
-    routes.push({
-        vehicleId: selectedVehicle.id,
-        originName: '',
-        originId: -1,
-        destinationName: '',
-        destinationId: -1,
-        dateOrigin: 0,
-        dateDestination: 0
-    });
+    const tr = $(element).closest('tr');
+    createRow(tr);
 }
 
 function onRemoveRableRow(event) {
     const element = event.srcElement;
-    const idIndex = element.dataset['routeId'];
-
-    if(routes.length < 2) {
-        alert('The route should have at least one node');
-        return;
-    }
-
-    const selectOrigin = document.getElementById(`OriginSelect${idIndex}`);
-    let selectOriginValue;
-    if(selectOrigin.tagName === 'SELECT') {
-        selectOriginValue = parseInt(selectOrigin.options[selectOrigin.selectedIndex].value);
-    } else {
-        selectOriginValue = parseInt(selectOrigin.dataset['dcId']);
-    }
-    const originIndex = distributionCenters.findIndex(distributionCenter => distributionCenter.id === selectOriginValue);
-    tmpDistributionCenters.push(distributionCenters[originIndex]);
-
-    const selectDestination = document.getElementById(`DestinationSelect${idIndex}`);
-    let selectDestinationValue;
-    if(selectDestination.tagName === 'SELECT') {
-        selectDestinationValue = parseInt(selectDestination.options[selectDestination.selectedIndex].value);
-    } else {
-        selectDestinationValue = parseInt(selectDestination.dataset['dcId']);
-    }
-    const destinationIndex = distributionCenters.findIndex(distributionCenter => distributionCenter.id === selectDestinationValue);
-    tmpDistributionCenters.push(distributionCenters[destinationIndex]);
-
     const td = element.parentNode;
     const tr = td.parentNode;
     const table = tr.parentNode;
     table.removeChild(tr);
-
-    routes.pop();
-    console.log('[ROUTES]', routes);
 }
 
-function createSelect(type) {
-    let element = null;
-    const idIndex = routes.length;
-    console.log('[ROUTES]', routes);
-    if(routes.length > 1 && type === 'OriginSelect') {
-        const route = routes[routes.length - 1];
-        element = document.createElement('input');
-        element.type = 'text';
-        element.value = route.destinationName;
-        element.dataset['dcId'] = route.originId;
-        element.readOnly = true;
-        element.id = `${type}${idIndex}`;
-        element.setAttribute('class', 'form-group');
-    } else {
-        element = document.createElement('select');
-        element.setAttribute('id', `${type}${idIndex}`);
-        element.setAttribute('class', 'form-group');
-        tmpDistributionCenters.forEach(distributionCenter => {
-            let option = document.createElement('option');
-            option.text = distributionCenter.name;
-            option.value = distributionCenter.id;
-            element.appendChild(option);
-        });
-    }
+function createSelect() {
+    let element = document.createElement('select');
+    element.setAttribute('class', 'form-group');
+    distributionCenters.forEach(distributionCenter => {
+        let option = document.createElement('option');
+        option.text = distributionCenter.name;
+        option.value = distributionCenter.id;
+        element.appendChild(option);
+    });
     return element;
 }
+
+$('#CreateRow').click(function() {
+    createRow();
+});
+
+$('#CreateRoute').click(function() {
+    let routes = [];
+    const trs = $('#RouteTable').children();
+    if(selectedVehicle == null) {
+        alert('Routes need a vehicle');
+        return;
+    }
+    if(trs.length <= 2) {
+        alert('You need at least two nodes to create a route');
+        return;
+    }
+
+    for (let i = 1; i < trs.length; i++) {
+        const tds = $(trs[i]).children();
+        
+        // SELECT ORIGIN
+        const selectOrigin = $(tds[0]).find('select');
+        const originValue = $(selectOrigin).find(':selected').val();
+
+        // SELECTDESTINATION
+        const selectDestination = $(tds[1]).find('select');
+        const destinationValue = $(selectDestination).find(':selected').val();
+
+        if(originValue === destinationValue) {
+            alert(`Error for route ${i}: Origin and destination location shouldn't be the same`);
+            return;
+        }
+        if(routes.length > 0) {
+            console.log('[ROUTES]', routes);
+            if(originValue !== routes[i - 2].destination) {
+                alert(`Error for route ${i}: Origin for route ${i} should be equal the origin of route ${i-1}`);
+                return;
+            }
+        }
+
+        // INPUT DATETIME-LOCAL ORIGIN
+        const dateOrigin = $(tds[2]).find('input');
+        const originDate = $(dateOrigin).val();
+        let originTimeStamp;
+        if(!originDate) {
+            alert(`Error for route ${i}: Date of origin should be set`);
+            return;
+        } else {
+            originTimeStamp = Date.parse(originDate)/1000;
+        }
+        
+
+        // INPUT DATETIME-LOCAL DESTINATION
+        const dateDestination = $(tds[3]).find('input');
+        destinationDate = $(dateDestination).val();
+        let destinationTimeStamp;
+        if(!destinationDate) {
+            alert(`Error for route ${i}: Date of destination should be set`);
+            return;
+        } else {
+            destinationTimeStamp = Date.parse(destinationDate)/1000;
+        }
+
+        if(destinationTimeStamp <= originTimeStamp) {
+            alert(`Error for route ${i}: Date of arrival should be bigger than origin`);
+        }
+        if(routes.length > 0) {
+            if(originTimeStamp <= routes[i-2].destinationDate) {
+                alert(`Error for route ${i}: Date of origin for route ${i} should be bigger than arrival for route ${i-1}`);
+            }
+        }
+
+        routes.push({
+            vehicleId: selectedVehicle.id,
+            origin: originValue,
+            destination: destinationValue,
+            originDate: originTimeStamp,
+            destinationDate: destinationTimeStamp
+        });
+    }
+
+    console.log('[ROUTES]', routes);
+});
