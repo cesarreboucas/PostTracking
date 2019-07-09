1cb5b9af0a365efad90678f24711bbffbe3ec3de
Oliveira Reboucas <oliveirareboucasc@student.douglascollege.ca>
2019-07-09 15:32:11 -0700

    fix javascript

--- src/main/java/com/PostTracking/Controllers/PackageController.java
+++ src/main/java/com/PostTracking/Controllers/PackageController.java
@@ -121,7 +121,6 @@ public class PackageController {
 		ArrayList<Path> paths = new ArrayList<Path>();
 		List<Route> routes = new ArrayList<Route>();
 		rDAO.findAll().iterator().forEachRemaining(routes::add);
-		System.out.println(routes.get(0));
 		int origin_id = 0;
 		int destination_id = 0;
 		try {
@@ -165,7 +164,6 @@ public class PackageController {
 		// Get the list of journeys ahead
 		List<Journey> journeys = jDAO.fetchFrom(new Timestamp (minimal));
 		System.out.println("Journey size : "+journeys.size());
-		//System.out.println("ID na lista (1): "+journeys.get(1).getVehicle().getId());
 		//Removing incomplete paths
 		for(int x=0; x < paths.size(); ++x) {
 			// If current position != destination, drop
