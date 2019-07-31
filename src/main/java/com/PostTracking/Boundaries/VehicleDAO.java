d7ba0f752dd2047d9c6c4e6c220d072e14efea8c
Marangoni <marangonim@student.douglascollege.ca>
2019-07-30 20:10:46 -0700

    Fixed a problem with the restart logic, fixed a problem with the datelocaltime input when trying to edit routes. Now when the user removes the routes from a vehicles it doesn't show on the page.

--- src/main/java/com/PostTracking/Boundaries/VehicleDAO.java
+++ src/main/java/com/PostTracking/Boundaries/VehicleDAO.java
@@ -16,5 +16,9 @@ import com.PostTracking.Entities.Vehicle;
 public interface VehicleDAO extends CrudRepository<Vehicle, Integer> {
 	@Query("SELECT v FROM Vehicle v WHERE v.deleted=false ORDER BY v.description ASC")
 	Iterable<Vehicle> fetchVehicles();
+	@Query("SELECT v FROM Vehicle v WHERE v.deleted=false " + 
+			"AND v.id NOT IN (SELECT r.vehicle.id FROM Route r " + 
+			"WHERE r.vehicle.id = v.id) ORDER BY v.description ASC")
+	Iterable<Vehicle> fetchVehiclesWithoutRoutes();
 	
 }
